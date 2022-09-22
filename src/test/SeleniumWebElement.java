e8a549101b92e7c2159110332d79a43d7d6a2d20
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-09-22 17:26:58 -0400

    Navigating the browser to another web page and to previous page, quitting the driver execution

--- src/test/SeleniumWebElement.java
+++ src/test/SeleniumWebElement.java
@@ -13,15 +13,26 @@ public class SeleniumWebElement {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.jobs/en/");
 
+        //maximizing the current page
+        driver.manage().window().maximize();
+
         // it pause the execution process for a few seconds
         Thread.sleep(4000);
 
         // it will search for Software Engineer and click the search button
         driver.findElement(By.id("id=search_typeahead-label")).sendKeys("Software Engineer");
-
         Thread.sleep(4000);
         driver.findElement(By.className("btn location-search form-control")).click();
         driver.findElement(By.linkText("Software Development")).click();
+
+        //navigating the browser to another web page
+        driver.navigate().to("https://github.com/radik-cs");
+        Thread.sleep(6000);
+        // navigating back to the previous web page
+        driver.navigate().back();
+
+        // quitting the driver execution
+        driver.quit();
 
 
     }
