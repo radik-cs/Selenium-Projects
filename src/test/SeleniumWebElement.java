40a4d2857ca7504273852be261c152866a85fd34
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-09-22 17:16:35 -0400

    created functions to search by id, class name, link text, click button, and pause an execution process for a few seconds

--- src/test/SeleniumWebElement.java
+++ src/test/SeleniumWebElement.java
@@ -11,7 +11,18 @@ public class SeleniumWebElement {
 
         System.setProperty("webdriver.chrome.driver","~Users/radikfakhretdinov/Downloads/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        driver.quit();
+        driver.get("https://www.amazon.jobs/en/");
+
+        // it pause the execution process for a few seconds
+        Thread.sleep(4000);
+
+        // it will search for Software Engineer and click the search button
+        driver.findElement(By.id("id=search_typeahead-label")).sendKeys("Software Engineer");
+
+        Thread.sleep(4000);
+        driver.findElement(By.className("btn location-search form-control")).click();
+        driver.findElement(By.linkText("Software Development")).click();
+
+
     }
 }
