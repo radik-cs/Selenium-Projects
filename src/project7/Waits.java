8f59b1b3d097551636a7f0e8e05e5bf0b5218edf
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-10-10 01:34:05 -0400

    Added the sendKeys and clickOn methods

--- src/project7/Waits.java
+++ src/project7/Waits.java
@@ -2,7 +2,11 @@ package project7;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -19,16 +23,37 @@ System.setProperty("webdriver.chrome.driver", "~Users/radikfakhretdinov/Download
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
 
-        driver.get("https://login.yahoo.com/");
+        // driver.get("https://login.yahoo.com/");
+        // driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("selen@yahoo.com");
+        // Thread.sleep(3000);
 
-        driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("selen@yahoo.com");
-        Thread.sleep(3000);
+        // driver.findElement(By.xpath("input[@id='login-signin']")).click();
 
-        driver.findElement(By.xpath("input[@id='login-signin']")).click();
-        
+        driver.get("https://www.facebook.com/");
 
+        WebElement firstname = driver.findElement(By.name("firstname"));
+        WebElement lastname = driver.findElement(By.name("lastname"));
 
+        sendKeys(driver,firstname,10,"Mike");
+        sendKeys(driver,lastname,20, "Tyson");
+
+        WebElement forgotAccount = driver.findElement((By.linkText("Forgotten Account?")));
+        clickOn((driver, forgotAccount,10));
 
 
     }
+
+
+    public static void sendKeys(WebDriver driver1, WebElement element, int timeout, String value){
+        new WebDriverWait(driver1,timeout).until(ExpectedConditions.visibilityOf(element));
+        element.sendKeys(value);
+    }
+
+    public static void clickOn(WebDriver driver1, WebElement element, int timeout){
+        new WebDriverWait((driver1, timeout).until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+
+
+
 }
