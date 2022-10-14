7e57783c204a9cd1303360966ff2c524b6bab524
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-10-13 23:15:40 -0400

    Created a function to throw an Exception for not existing element

--- src/project9/HandleExceptions.java
+++ src/project9/HandleExceptions.java
@@ -1,7 +1,21 @@
 package project9;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HandleExceptions {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "~Users/radikfakhretdinov/Downloads/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+        Thread.sleep(2000);
+
+        // this element is not existing and it will throw an Exception
+        driver.findElement(By.name("fake")).click();
+
+        // if we want to handle it
+
     }
 }
