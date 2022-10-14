05abfeda1d65ee592f96ab2e98d36c3cb8d9ff43
radik-cs <84479742+radik-cs@users.noreply.github.com>
2022-10-13 23:22:12 -0400

    try-catch block to handle the Exception

--- src/project9/HandleExceptions.java
+++ src/project9/HandleExceptions.java
@@ -1,6 +1,7 @@
 package project9;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -12,10 +13,22 @@ public class HandleExceptions {
         driver.get("https://www.google.com/");
         Thread.sleep(2000);
 
+        // # 1
         // this element is not existing and it will throw an Exception
-        driver.findElement(By.name("fake")).click();
+        // driver.findElement(By.name("fake")).click();
 
+        // #2
         // if we want to handle it
+        try {
+            driver.findElement(By.name("fake")).click();
+        }catch (NoSuchElementException e){
+            System.out.println("This element is not found");
+            System.out.println("Hello");
+
+            // thrown (e);
+        }
+
+        System.out.println("Hello");
 
     }
 }
